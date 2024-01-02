ebe0ff9293c5d9e768ddd05ae4067142ad86c939
Kenedy Kaefer <kenedykae@gmail.com>
2024-01-02 20:41:40 -0300

    Add kyrename class for file system manipulation
    Introduced kyrename class with nested FileSystem, FileSystemItem, and FileSystemCollection classes. The kyrename class manages multiple file systems, represented as a vector of unique pointers to FileSystem objects. FileSystemItem represents a single item in a file system, while FileSystemCollection represents a collection of file system items. Each class has appropriate constructors to initialize their respective data members.

--- src/kyrename.hpp
+++ src/kyrename.hpp
@@ -2,13 +2,51 @@
 #define KYRENAME_HPP
 
 #include <string>
+#include <filesystem>
+#include <vector>
 
 namespace ky
 {
+    namespace fs = std::filesystem;
+
     std::wstring remove_accents(std::wstring const &str);
     std::wstring to_lower(std::wstring const &str);
     std::wstring to_alphanum(std::wstring const &str, wchar_t const separator);
     std::wstring normalize_separator(std::wstring const &str, wchar_t const separator, std::wstring const &separators_to_replace = L" ,.?;:[]{}|()-_");
+
+    class kyrename
+    {
+    public:
+        kyrename(std::vector<fs::path> const &path);
+
+    private:
+        class FileSystem
+        {
+        public:
+            virtual ~FileSystem() = default;
+        };
+
+        class FileSystemItem : public FileSystem
+        {
+        public:
+            FileSystemItem(fs::path const &item_path);
+
+        private:
+            fs::path m_item_path;
+        };
+
+        class FileSystemCollection : public FileSystem
+        {
+        public:
+            FileSystemCollection(fs::path const &directory_path);
+
+        private:
+            fs::path m_directory_path;
+            std::vector<FileSystemItem> m_items;
+        };
+
+        std::vector<std::unique_ptr<FileSystem>> m_file_systems;
+    };
 
 } // namespace ky
 
