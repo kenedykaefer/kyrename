e682a53b3bf9441bad277e70aca79a0033dd28f8
Kenedy Kaefer <kenedykae@gmail.com>
2024-01-02 07:48:59 -0300

    Refactor ky::remove_accents, ky::to_lower, ky::to_alphanum, and ky::normalize_separator to be member functions of ky::FileSystemItem

--- src/kyrename.hpp
+++ src/kyrename.hpp
@@ -8,7 +8,6 @@
 namespace ky
 {
     namespace fs = std::filesystem;
-
     struct normalize_name_options
     {
         bool accents;
@@ -17,21 +16,22 @@ namespace ky
         wchar_t separator;
     };
 
-    struct file_name
+    class FileSystemItem
     {
-        fs::path path;
-        std::wstring filename;
-        std::wstring extension;
-        bool hidden_file;
-    };
-
-    std::wstring remove_accents(std::wstring const &str);
-    std::wstring to_lower(std::wstring const &str);
-    std::wstring to_alphanum(std::wstring const &str, wchar_t const separator);
-    std::wstring normalize_separator(std::wstring const &str, wchar_t const separator, std::wstring const &separators_to_replace = L" ,.?;:[]{}|()-_");
-    file_name normalize_name(file_name name, normalize_name_options const &options);
-
-    std::vector<ky::file_name> get_files(std::vector<fs::path> const &paths);
+    public:
+        FileSystemItem(fs::path const &path);
+        std::wstring remove_accents(std::wstring const &str);
+        std::wstring to_lower(std::wstring const &str);
+        std::wstring to_alphanum(std::wstring const &str, wchar_t const separator);
+        std::wstring normalize_separator(std::wstring const &str, wchar_t const separator, std::wstring const &separators_to_replace = L" ,.?;:[]{}|()-_");
+        void normalize_name(normalize_name_options const &options);
+        
+    private:
+        fs::path parent_path;
+        fs::path file_name;
+        fs::path new_file_name;
+    }; 
+    
 } // namespace ky
 
 #endif // KYRENAME_HPP
