a857b7eceafaa4009dbed4b744db2886bbd67531
Kenedy Kaefer <kenedykae@gmail.com>
2024-01-07 10:47:26 -0300

    Remove unused code in main.cpp, kyrename.hpp and kyrename.cpp

--- src/kyrename.hpp
+++ src/kyrename.hpp
@@ -1,7 +1,7 @@
 #ifndef KYRENAME_HPP
 #define KYRENAME_HPP
 
-#include <string>
+#include <string_toolkit.hpp>
 #include <filesystem>
 #include <vector>
 
@@ -9,29 +9,6 @@ namespace ky
 {
     namespace fs = std::filesystem;
 
-    struct normalize_name_options
-    {
-        bool accents;
-        bool lower;
-        bool alphanum;
-        wchar_t separator;
-    };
-
-    struct file_name
-    {
-        fs::path path;
-        std::wstring filename;
-        std::wstring extension;
-        bool hidden_file;
-    };
-
-    std::wstring remove_accents(std::wstring const &str);
-    std::wstring to_lower(std::wstring const &str);
-    std::wstring to_alphanum(std::wstring const &str, wchar_t const separator);
-    std::wstring normalize_separator(std::wstring const &str, wchar_t const separator, std::wstring const &separators_to_replace = L" ,.?;:[]{}|()-_");
-    file_name normalize_name(file_name name, normalize_name_options const &options);
-
-    std::vector<ky::file_name> get_files(std::vector<fs::path> const &paths);
 } // namespace ky
 
 #endif // KYRENAME_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,8 @@
 #include <kyrename.hpp>
 #include <stdexcept>
 #include <filesystem>
-#include <vector>
 
 namespace fs = std::filesystem;
-
-void print_names_with_new_names(std::vector<ky::file_name> const &names, std::vector<ky::file_name> const &new_names)
-{
-    std::cout << "Old name\tNew name" << std::endl;
-    std::cout << "--------\t--------" << std::endl;
-
-    for (size_t i = 0; i < names.size(); ++i)
-    {
-        std::wcout << names[i].filename << names[i].extension << "\t\t";
-        if (names[i].hidden_file)
-        {
-            std::wcout << L".";
-        }
-        std::wcout << new_names[i].filename << new_names[i].extension << std::endl;
-    }
-}
 
 int main(int argc, char **argv)
 {
@@ -30,24 +13,6 @@ int main(int argc, char **argv)
         {
             throw std::invalid_argument("Usage: " + std::string(argv[0]) + " <path>");
         }
-
-        std::vector<fs::path> paths;
-
-        for (int i = 1; i < argc; ++i)
-        {
-            paths.push_back(fs::path(argv[i]));
-        }
-
-        std::vector<ky::file_name> names = ky::get_files(paths);
-        std::vector<ky::file_name> new_names;
-
-        for (auto const &name : names)
-        {
-            ky::file_name new_name = ky::normalize_name(name, {true, true, true, L'_'});
-            new_names.push_back(new_name);
-        }
-
-        print_names_with_new_names(names, new_names);
     }
     catch (const std::exception &e)
     {
