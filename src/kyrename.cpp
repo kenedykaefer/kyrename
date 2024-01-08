cfb9df7e9dedfc1812931ee37b49522d09bc2d2b
Kenedy Kaefer <kenedykae@gmail.com>
2024-01-08 08:29:28 -0300

    Add kyrename class with add_path and print_paths functions

--- src/kyrename.cpp
+++ src/kyrename.cpp
@@ -1,2 +1,40 @@
 #include <kyrename.hpp>
+#include <iostream>
+#include <algorithm>
 
+ky::kyrename::kyrename()
+{
+}
+
+void ky::kyrename::add_path(const fs::path &path)
+{
+    if (!fs::exists(path))
+    {
+        throw std::invalid_argument("Path " + path.string() + " does not exist.");
+    }
+
+    fs::path canonicalPath = fs::canonical(path);
+
+    if (canonicalPath == fs::current_path())
+    {
+        throw std::invalid_argument("Cannot rename current directory.");
+    }
+
+    if (canonicalPath == fs::current_path().parent_path())
+    {
+        throw std::invalid_argument("Cannot rename parent directory.");
+    }
+
+    if (std::find(m_paths.begin(), m_paths.end(), path) == m_paths.end())
+    {
+        m_paths.push_back(path);
+    }
+}
+
+void ky::kyrename::print_paths() const
+{
+    for (const auto &path : m_paths)
+    {
+        std::wcout << path.wstring() << std::endl;
+    }
+}
--- src/kyrename.hpp
+++ src/kyrename.hpp
@@ -4,10 +4,22 @@
 #include <string_toolkit.hpp>
 #include <filesystem>
 #include <vector>
+#include <stdexcept>
 
 namespace ky
 {
     namespace fs = std::filesystem;
+
+    class kyrename
+    {
+    public:
+        kyrename();
+        void add_path(const fs::path& path);
+        void print_paths() const;
+
+    private:
+        std::vector<fs::path> m_paths;
+    };
 
 } // namespace ky
 
